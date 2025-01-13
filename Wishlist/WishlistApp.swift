d414b39b8d765a9c95b8a0eff6e8e2ed2db29199
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-13 15:56:21 +0300

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Fatih Emre on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
